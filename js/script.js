ba7d7269eedfef431842f79308b2eaeb908d8ca6
Kirill <batomunkuevkirill@yandex.ru>
2022-04-02 20:36:52 +0700

    Adaptive main-screen

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 function ibg() {
-
 	let ibg = document.querySelectorAll(".ibg");
 	for (var i = 0; i < ibg.length; i++) {
-		if (ibg[i].querySelector('img')) {
-			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		if (ibg[i].querySelector("img")) {
+			ibg[i].style.backgroundImage =
+				"url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
 		}
 	}
 }
 
 ibg();
+
+const headerBurger = document.querySelector(".header__burger");
+const menuList = document.querySelector(".menu__list");
+const menuLink = menuList.querySelectorAll('.menu__link');
+const body = document.body;
+
+headerBurger.addEventListener("click", () => {
+	headerBurger.classList.toggle("header__burger_active");
+	menuList.classList.toggle("menu__list_active");
+	menuLink.forEach(link => {
+		link.addEventListener('click', () => {
+			headerBurger.classList.remove("header__burger_active");
+			menuList.classList.remove("menu__list_active");
+		});
+	});
+	body.classList.toggle("lock");
+});
